0dd7dafecd93ed9e047e72495903856421b31686
Th-Fauconnier <fauconthierry@outlook.fr>
2021-01-17 14:35:22 +0100

    Update OrganizationController
    adding logging to dev.log
    simplification error Response
    use of userInterface

--- src/Controller/OrganizationController.php
+++ src/Controller/OrganizationController.php
@@ -10,7 +10,7 @@ use App\Service\Request\RequestParameters;
 use App\Service\Security\RequestSecurity;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
-use PhpParser\Node\Param;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,21 +27,24 @@ class OrganizationController extends AbstractController
 
     private ParametersValidator $paramValidator;
 
+    private LoggerInterface $logger;
+
     /**
      * UserController constructor.
      * @param RequestSecurity $requestSecurity
      * @param RequestParameters $requestParameters
      * @param EntityManagerInterface $entityManager
      * @param ParametersValidator $paramValidator
+     * @param LoggerInterface $logger
      */
-    public function __construct(RequestSecurity $requestSecurity, RequestParameters $requestParameters, EntityManagerInterface $entityManager, ParametersValidator $paramValidator){
+    public function __construct(RequestSecurity $requestSecurity, RequestParameters $requestParameters, EntityManagerInterface $entityManager, ParametersValidator $paramValidator, LoggerInterface $logger){
         $this->requestSecurity = $requestSecurity;
         $this->requestParameters = $requestParameters;
         $this->entityManager = $entityManager;
         $this->paramValidator = $paramValidator;
+        $this->logger = $logger;
     }
 
-    //todo access role?
     /**
      * @Route("/organization/create", name="create_organization", methods="post")
      * @param Request $request
@@ -52,9 +55,10 @@ class OrganizationController extends AbstractController
         //cleanXSS
         try{
             $request = $this->requestSecurity->cleanXSS($request);
-        }catch(SecurityException $exception){
+        }catch(SecurityException $e){
+            $this->logger->warning($e);
             return new Response(
-                json_encode(["success" => false, "error" => "Potential attack has been detected"]),
+                json_encode(["error" => "ACCESS_FORBIDDEN"]),
                 Response::HTTP_FORBIDDEN,
                 ["Content-Type" => "application/json"]);
         }
@@ -62,64 +66,39 @@ class OrganizationController extends AbstractController
         //recover parameters of the request in an array $data
         $data = $this->requestParameters->getData($request);
 
-        //request's required & optional Field
-        $requiredFields = ["userId", "type", "name", "email"];
-        $optionalFields = ["phone", "address"];
-
         //validation user's id and recover userObject
         try{
-            if (!isset($data['userId'])) {
-                throw new Exception("UserId required for new organization's referent", Response::HTTP_BAD_REQUEST);
-            }
-            if(!is_numeric($data['userId'])){
-                throw new Exception("userId must be numeric", Response::HTTP_BAD_REQUEST);
-            }
-
-            $user = $this->entityManager->getRepository(User::class)->find($data["userId"]);
+            $user = $this->entityManager->getRepository(User::class)->find($this->getUser()->getId());
             if ($user == null) {
-                throw new Exception("user not found", Response::HTTP_NOT_FOUND);
+                return new Response(
+                    json_encode(["error" => "DATA_NOT_FOUND"]),
+                    Response::HTTP_NOT_FOUND,
+                    ["Content-Type" => "application/json"]
+                );
             }
         }catch(\Exception $e){
+            $this->logger->error($e);
             return new Response(
-                json_encode(["success" => false, "error" => $e->getMessage()]),
-                $e->getCode(),
+                json_encode(["error" => "ERROR_SERVER"]),
+                Response::HTTP_INTERNAL_SERVER_ERROR,
                 ["Content-Type" => "application/json"]
             );
         }
 
-        //create user object
-        $org = new Organization();
+        //todo check email non unique mais unique par name
+        //request's required & optional Field
+        $requiredFields = ["type", "name", "email"];
+        $optionalFields = ["phone", "address"];
+        $this->paramValidator->initValidator($requiredFields, $optionalFields,Organization::class, $data);
 
         //Validate required fields
         try{
-            $violationsList = $violationsList = $this->paramValidator->fieldsValidation(
-                "organization",
-                $requiredFields,
-                true,
-                $data
-            );
+            $violationsList = $this->paramValidator->checkViolations();
         }catch(Exception $e){
+            $this->logger->error($e);
             return new Response(
-                json_encode(["success" => false, "error" => $e->getMessage()]),
-                Response::HTTP_BAD_REQUEST,
-                ["Content-Type" => "application/json"]
-            );
-        }
-
-        //validate optional field
-        try{
-            $violationsList = array_merge(
-                $violationsList,
-                $violationsList = $this->paramValidator->fieldsValidation(
-                    "organization",
-                    $optionalFields,
-                    false,
-                    $data)
-            );
-        }catch(Exception $e){
-            return new Response(
-                json_encode(["success" => false, "error" => $e->getMessage()]),
-                Response::HTTP_BAD_REQUEST,
+                json_encode(["error" => "ERROR_SERVER"]),
+                Response::HTTP_INTERNAL_SERVER_ERROR,
                 ["Content-Type" => "application/json"]
             );
         }
@@ -127,11 +106,14 @@ class OrganizationController extends AbstractController
         //return violations
         if( count($violationsList) > 0 ){
             return new Response(
-                json_encode(["success" => false, "error" => $violationsList]),
+                json_encode(["error" => $violationsList]),
                 Response::HTTP_BAD_REQUEST,
                 ["Content-Type" => "application/json"]
             );
         }
+
+        //create user object
+        $org = new Organization();
 
         //set organization's validated fields
         $org->setReferent($user);
@@ -152,13 +134,15 @@ class OrganizationController extends AbstractController
             $this->entityManager->persist($org);
             $this->entityManager->flush();
         }catch(Exception $e){
+            $this->logger->error($e);
             return new Response(
-                json_encode(["success" => false, "error" => $e->getMessage()]),
+                json_encode(["error" => "ERROR_SERVER"]),
                 $e->getCode(),
                 ["Content-Type" => "application/json"]
             );
         }
 
+        $this->logger->info("User with id : " .$user->getId(). " has created an Organization with id : " .$org->getId());
         return new Response(
             json_encode(["success" => true]),
             Response::HTTP_OK,
@@ -167,7 +151,7 @@ class OrganizationController extends AbstractController
     }
 
     /**
-     * @Route("/organization", name="get_organization", methods="get")
+     * @Route("/organization", name="get_refered_organization", methods="get")
      * @param Request $request
      * @return Response
      */
@@ -176,8 +160,9 @@ class OrganizationController extends AbstractController
         try {
             $request = $this->requestSecurity->cleanXSS($request);
         }catch(\Exception $e) {
+            $this->logger->warning($e);
             return new Response(
-                json_encode(["success" => false, "error" => "Potential attack has been detected"]),
+                json_encode(["error" => "ACCESS_FORBIDDEN"]),
                 Response::HTTP_FORBIDDEN,
                 ["Content-Type" => "application/json"]);
         }
@@ -185,37 +170,42 @@ class OrganizationController extends AbstractController
         //recover parameters of the request in an array $data
         $data = $this->requestParameters->getData($request);
 
-        $orgRepository = $this->entityManager->getRepository(Organization::class);
         try{
-            //verifies the existence of userId or email field for query by criteria
-            if(count($data) > 0 && (isset($data['id']) || isset($data['email']))){
-                $orgData = $orgRepository->findBy(
-                    isset($data['id']) ? ['id' => $data['id']] : ['email' => $data['email']]
-                );
-            }else { //otherwise we return all users
-                $orgData = $orgRepository->findAll();
+            if(isset($data['id']) && !is_numeric($data['id'])){
+                throw new Exception("id parameter must be numeric", Response::HTTP_BAD_REQUEST);
             }
-        }
-        catch(\Exception $e){
+        }catch(\Exception $e){
+            $this->logger->info($e);
             return new Response(
-                json_encode(["success" => false, "error" => $e->getMessage()]),
+                json_encode(["error" => $e->getMessage()]),
                 $e->getCode(),
                 ["Content-Type" => "application/json"]
             );
         }
 
-        //serialize all found userObject
+        $orgRepository = $this->entityManager->getRepository(Organization::class);
+        try{
+            //query for organizationData
+            if(count($data) > 0 && (isset($data['id']))){
+                $orgData = $orgRepository->findBy(['id' => $data['id']]);
+            }else { //otherwise we return all users
+                $orgData = $orgRepository->findAll();
+            }
+        }
+        catch(\Exception $e){
+            $this->logger->error($e);
+            return new Response(
+                json_encode(["error" => "ERROR_SERVER"]),
+                $e->getCode(),
+                ["Content-Type" => "application/json"]
+            );
+        }
+
+        //serialize all found data
         if (count($orgData) > 0 ) {
             foreach($orgData as $key => $org){
                 $orgData[$key] = $org->serialize("read_organization");
             }
-        }
-        else {
-            return new Response(
-                json_encode(["success" => false, "error" => "Organization Not Found"]),
-                Response::HTTP_NOT_FOUND,
-                ["Content-Type" => "application/json"]
-            );
         }
 
         //success
@@ -225,4 +215,44 @@ class OrganizationController extends AbstractController
             ["content-type" => "application/json"]
         );
     }
+
+    /*public function getReferedOrganization(Request $request){
+        //cleanXSS
+        try {
+            $request = $this->requestSecurity->cleanXSS($request);
+        }catch(\Exception $e) {
+            $this->logger->warning($e);
+            return new Response(
+                json_encode(["error" => "ACCESS_FORBIDDEN"]),
+                Response::HTTP_FORBIDDEN,
+                ["Content-Type" => "application/json"]);
+        }
+
+        $orgRepository = $this->entityManager->getRepository(Organization::class);
+        try{
+            $orgData = $orgRepository->findBy(['referent' => $this->getUser()->getId()]);
+        }
+        catch(\Exception $e){
+            $this->logger->error($e);
+            return new Response(
+                json_encode(["error" => "ERROR_SERVER"]),
+                $e->getCode(),
+                ["Content-Type" => "application/json"]
+            );
+        }
+
+        //serialize all found userObject
+        if (count($orgData) > 0 ) {
+            foreach($orgData as $key => $org){
+                $orgData[$key] = $org->serialize("read_referent");
+            }
+        }
+
+        //success
+        return new Response(
+            json_encode(["data" => $orgData]),
+            Response::HTTP_OK,
+            ["content-type" => "application/json"]
+        );
+    }*/
 }
